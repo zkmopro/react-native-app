a845d1f8f2740b80c5757d9f50022e027d2b98f9
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-10-22 21:47:48 -0700

    refactor: remove obsolete mopro module files and update .gitignore
    - Deleted unused JavaScript and TypeScript files from the mopro module, including index.js, NativeMyTestLibrary.js, and generated files.
    - Updated .gitignore to include additional directories for better exclusion of build artifacts.
    - Adjusted project structure to enhance maintainability and clarity.

--- modules/mopro/lib/typescript/src/NativeMyTestLibrary.d.ts
+++
@@ -1,8 +0,0 @@
-import type { TurboModule } from 'react-native';
-export interface Spec extends TurboModule {
-    installRustCrate(): boolean;
-    cleanupRustCrate(): boolean;
-}
-declare const _default: Spec;
-export default _default;
-//# sourceMappingURL=NativeMyTestLibrary.d.ts.map
--- modules/mopro/lib/typescript/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './generated/mopro_example_app';
-import * as mopro_example_app from './generated/mopro_example_app';
-export declare function uniffiInitAsync(): Promise<void>;
-declare const _default: {
-    mopro_example_app: typeof mopro_example_app;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
